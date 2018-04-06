3b1c5bc0fca2f57fa4eee723c0b233fa25c316d4
ardak <ardak.shalkar@gmail.com>
2018-04-06 16:37:36 +0600

    Correcting works

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import AppDraft from './AppDraft';
-import AppDraft2 from './AppDraft2';
-import AppDraftCorrect from './AppDraftCorrect';
-import AppDraftDecorator from './AppDraftDecorator';
+//import App from './App';
+//import AppDraft from './AppDraft';
+//import AppDraft2 from './AppDraft2';
+//import AppDraftCorrect from './AppDraftCorrect';
+//import AppDraftDecorator from './AppDraftDecorator';
+import AppCorrector from './AppCorrector';
 import './index.css';
 
 ReactDOM.render(
-  <AppDraft2/>,
+  <AppCorrector/>,
   document.getElementById('root')
 );
